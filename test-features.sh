c92107f7f34d19b553c0714a200dbee833e35abe
Markus Kolb <github@tower-net.de>
2023-10-09 12:02:02 +0200

    test: test-feature.sh with --all-features

--- test-features.sh
+++ test-features.sh
@@ -3,9 +3,11 @@
 features="migration migration_embed ureq ureq_tls ureq_webpki"
 features_addon="log monitor percent_encoding tracing ureq_charset ureq_socks_proxy url"
 
+
 cargo_test() {
     feature="$1"
     addon="$2"
+    all_features="$3"
 
     rc=0
     if [ -n "$feature" ] ; then
@@ -21,15 +23,17 @@ cargo_test() {
             rc="$?"
         }
     else
+        [ -n "$all_features" ] && all_features="--all-features"
+
         [ -n "$clippy" ] && {
-            echo "$0: $clippy --no-deps -- -Dwarnings"
-            $clippy --no-deps -- -Dwarnings
+            echo "$0: $clippy --no-deps $all_features -- -Dwarnings"
+            $clippy --no-deps $all_features -- -Dwarnings
             rc="$?"
         }
 
         [ "$rc" -eq 0 ] && {
-            echo "$0: cargo test"
-            cargo test
+            echo "$0: cargo test $all_features"
+            cargo test $all_features
             rc="$?"
         }
     fi
@@ -39,6 +43,7 @@ cargo_test() {
         exit "$rc"
     }
 }
+
 
 script_dir="$(dirname "$0")"
 script_dir="$(cd "$script_dir" && pwd)"
@@ -50,6 +55,8 @@ $clippy -h >/dev/null 2>&1
 [ "$?" -eq 0 ] || clippy=""
 
 cargo_test
+
+cargo_test "" "" --all-features
 
 for f in $features ; do
     cargo_test "$f"
